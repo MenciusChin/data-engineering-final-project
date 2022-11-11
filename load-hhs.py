24f29f989ca91f0e75e20bfd337a884e7eef5337
MenciusChin <menciuschin@gmail.com>
2022-11-10 22:02:15 -0500

    test ignore

--- load-hhs.py
+++ load-hhs.py
@@ -1,6 +1,12 @@
 """Module for loading HHS data into DB"""
 
+import sys
+import pandas as pd
 from credentials import DB_USER, DB_PASSWORD
 
+# Store credentials
 username = DB_USER
 password = DB_PASSWORD
+
+# Load data from terminal input
+data = pd.read_csv('data/hhs/' + sys.agrv[1])
